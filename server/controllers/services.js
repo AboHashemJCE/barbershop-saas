6334ded54f8adae3cb516fc79469ead5ad63372e
AboHashemJCE <moody_ashhab@hotmail.com>
2026-05-30 12:56:04 +0300

    no change

--- server/controllers/services.js
+++ server/controllers/services.js
@@ -165,7 +165,7 @@ export const updateService = async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: 'Server error', error: error.message });
   }
-;
+};
 
 // ----------------------------
 // DELETE /api/services/:id
